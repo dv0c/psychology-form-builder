b0e64253bd40937b16332d45e1082396b7f72f68
dv0c <tasosmeidanis12@gmail.com>
2024-06-28 09:52:30 +0300

    modified:   app/api/tasks/[taskId]/route.ts 	modified:   app/api/tasks/route.ts

--- app/api/tasks/[taskId]/route.ts
+++ app/api/tasks/[taskId]/route.ts
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function GET(res: Response, { params }: { params: { taskId: string } }) {
   if (!params?.taskId) return NextResponse.json("error");
-        
+
   const _$ = await prisma.form.findUnique({
     where: {
       shareURL: params.taskId,
     },
   });
 
-    if (!_$) return NextResponse.json("error");
+  if (!_$) return NextResponse.json("error");
   return NextResponse.json(_$);
 }
--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(res:Response) {
-    const tasks = await prisma.form.findMany()
-    return NextResponse.json(tasks)
+export async function GET(req: Request) {
+    const tasks = await prisma.form.findMany();
+    return NextResponse.json(tasks);
 }
