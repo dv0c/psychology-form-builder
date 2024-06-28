dc848a4bd46c3f19003ac0b51fb43dbc5ab54b7c
dv0c <tasosmeidanis12@gmail.com>
2024-06-28 09:52:45 +0300

    modified:   app/api/tasks/route.ts

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: Request, res:Response) {
     const tasks = await prisma.form.findMany();
     return NextResponse.json(tasks);
 }
