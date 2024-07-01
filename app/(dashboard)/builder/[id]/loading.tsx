61badccd79ff23f37470ffabe0b15cbbccfb2362
dv0c <tasosmeidanis12@gmail.com>
2024-07-01 09:29:11 +0300

    modified:   app/(dashboard)/builder/[id]/loading.tsx

--- app/(dashboard)/builder/[id]/loading.tsx
+++ app/(dashboard)/builder/[id]/loading.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ImSpinner2 } from "react-icons/im";
+import { LuLoader2 } from "react-icons/lu";
 
 function Loading() {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <ImSpinner2 className="animate-spin h-12 w-12" />
+      <LuLoader2 className="animate-spin h-12 w-12" />
+      Loading the builder
     </div>
   );
 }
